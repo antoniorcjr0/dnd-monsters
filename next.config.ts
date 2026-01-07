77e8a0654d2748d794f3bdb4fa13c855e5e81f7e
Antonio Ramos <your_private_email@example.com>
2026-01-07 14:46:08 +0000

    chore: allow dnd5eapi images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.dnd5eapi.co",
+        pathname: "/api/images/monsters/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
